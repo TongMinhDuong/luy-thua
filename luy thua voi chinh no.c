dac9e58087ea7fa7e49c9c716a8599d960f7e01b
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-13 10:17:02 +0700

    Add files via upload

---
+++ luy thua voi chinh no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a;
+	int b;
+	
+	printf(" tinh b khi a = ", a);
+	scanf("%d", &a);
+	b= pow(a,a);
+	printf(" khi a = %d thi b = %d", a,b);
+	return 0;
+}
---
+++ luythua.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int x=4;
+	int y=4;
+	int z= pow(x,y);
+
+	printf(" z = %d\n", z);
+
+	return 0;
+}
